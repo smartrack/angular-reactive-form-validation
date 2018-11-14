2a9c2876482fb000c95e552cc03f0e0cb503dcc4
jagadesh kannan <jaga02it010@gmail.com>
2018-11-14 00:45:46 -0500

    Removing empty scss file

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -3,8 +3,7 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  templateUrl: './login.component.html'
 })
 export class LoginComponent implements OnInit {
 
